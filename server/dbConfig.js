c70a7f5a6f629d1dfb929dba5f3d1d7416e58e20
Nicholas W Fortner <nwfortner@gmail.com>
2016-12-13 14:09:37 -0800

    remove db config

--- server/dbConfig.js
+++
@@ -1,6 +0,0 @@
-//This is the file to paste mongo variables into//
-module.exports = {
- user: 'legacy',
- password: 'goals',
- path: 'mongodb://legacy:goals@ds133348.mlab.com:33348/get-hassled'
-};
