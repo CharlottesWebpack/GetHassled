36c5c0866a712066dc014dd77d842682118682f7
Nicholas W Fortner <nwfortner@gmail.com>
2016-12-13 13:40:08 -0800

    stuff

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -1,1 +1,6 @@
 //This is the file to paste mongo variables into//
+module.exports = {
+ user: 'legacy',
+ password: 'goals',
+ path: 'mongodb://legacy:goals@ds133348.mlab.com:33348/get-hassled'
+};
