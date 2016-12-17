b6b9b44e48ee298e70df6b65a7cb62a32268f472
nikshalavelayutham <niky.vel@gmail.com>
2016-12-17 10:34:31 -0800

    [email](server)Setup spam emails based on the grade{NV}

--- server/userModel.js
+++ server/userModel.js
@@ -5,7 +5,8 @@ var userSchema = mongoose.Schema()
 userSchema.add({
   token: String, // facebook token
   id: String, // facebook or google id
-  name: String, // facebook display name
+  name: String,
+  email:String, // facebook display name
   phoneNumber: String, // user phone number
   buddyName: String,  // accountability buddy name
   buddyPhone: String,  // accountability buddy phone number
